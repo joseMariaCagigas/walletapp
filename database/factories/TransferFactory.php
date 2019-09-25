34499a49847a5c971b19ae7d617a4c04bf66e84d
mantamusica <cccfacebuk@gmail.com>
2019-09-25 10:09:56 +0200

    generados modelos, migraciones y factories

---
+++ database/factories/TransferFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Transfer::class, function (Faker $faker) {
+    return [
+       'description' => $faker->text($maxNbChars = 100),
+        'amount' => $faker->numberBetween($min = 100, $max = 90),
+        'wallet_id' => $faker->randomDigitNotNull
+    ];
+});
